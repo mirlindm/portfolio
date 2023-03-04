e343b03a8012c3f7b1e2bf8c9f2c16e301d01774
mirlind.murati <mirlind.murati@bigbank.ee>
2023-03-04 18:00:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let theme = localStorage.getItem('theme');
+let themeDots = document.getElementsByClassName('theme-dot');
+
+if (theme === null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode;
+        setTheme(mode);
+    })
+}  
+
+function setTheme(mode) {
+    if(mode === 'light') document.getElementById('theme-style').href = 'default.css'
+    if(mode === 'blue') document.getElementById('theme-style').href = 'themes/blue.css'
+    if(mode === 'green') document.getElementById('theme-style').href = 'themes/green.css'
+    if(mode === 'purple') document.getElementById('theme-style').href = 'themes/purple.css'
+    localStorage.setItem('theme', mode);
+}

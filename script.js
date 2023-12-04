197a4ca0a5c8e88afa6ce925a4d0c61bfe1a3e76
Mirlind Murati <mirlindmurati777@gmail.com>
2023-12-04 15:20:55 +0200

    no script needed

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for (var i = 0; themeDots.length > i; i++) {
 }  
 
 function setTheme(mode) {
-    if(mode === 'light') document.getElementById('theme-style').href = 'default.css'
+    //if(mode === 'light') document.getElementById('theme-style').href = 'default.css'
     if(mode === 'blue') document.getElementById('theme-style').href = 'themes/blue.css'
     if(mode === 'green') document.getElementById('theme-style').href = 'themes/green.css'
     if(mode === 'purple') document.getElementById('theme-style').href = 'themes/purple.css'
